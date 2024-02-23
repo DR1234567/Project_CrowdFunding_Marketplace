353d72d42fc732550c4c1788b16b8d1252af2b5e
Divyansh <rawatdivyansh458@gmail.com>
2024-02-23 11:38:21 +0530

    first commit

---
+++ Context/contants.js
@@ -0,0 +1,5 @@
+import crowdFunding from "./CrowdFunding.json";
+
+//Crowd Funding MARKETPALCE
+export const CrowdFundingAAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+export const CrowdFundingABI = crowdFunding.abi;
